346b5eb517e86aea97cafc9c1befd5833c1d8663
BorisPanich <panich2303@gmail.com>
2021-11-26 09:56:35 +0300

    connected state in storybook from decorator

--- src/AppWithRedux.stories.tsx
+++ src/AppWithRedux.stories.tsx
@@ -3,19 +3,17 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import AppWithRedux from './AppWithRedux';
 import { store } from './state/store';
+import { ReduxStoreProviderDecorator } from './stories/ReduxStoreProviderDecorator';
 
 
 export default {
     title: 'Todolist/AppWithRedux',
     component: AppWithRedux,
-    args: {
-
-    },
+    args: {},
+    decorators: [ReduxStoreProviderDecorator]       //что-то вроде HOC
 } as ComponentMeta<typeof AppWithRedux>;
 
-const Template: ComponentStory<typeof AppWithRedux> = () => <Provider store={store}>
-    <AppWithRedux />
-</Provider>;
+const Template: ComponentStory<typeof AppWithRedux> = () => <AppWithRedux />
 
 export const AppWithReduxStories = Template.bind({});
 AppWithReduxStories.args = {};
